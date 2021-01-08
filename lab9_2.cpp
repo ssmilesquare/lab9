34406c3cf958d07e5911ee6e7101f0b0408df232
ssmilesquare <ssmilesquare@gmail.com>
2021-01-08 17:36:35 +0700

    lab9_2 completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -11,6 +11,17 @@ int main (){
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------" << endl;
+	dest << "AR CHI\n";
+	dest << "AR BOW\n";
+	dest << "CHOW CHOW\n";
+	dest << "BA KA\n";
+	dest << "BA GEAR\n";
+	dest << "CHEER CHEER\n";
+	dest << "WE ARE ALL\n";
+	dest << "ENTANEER\n";
+	dest << "CMU\n";
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
